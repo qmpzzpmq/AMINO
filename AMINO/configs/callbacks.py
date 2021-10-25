b81bd88a971fb422d0555bae97f37ed9fe6cde04
qmpzzpmq <405691733@qq.com>
2021-10-25 13:55:55 +0800

    update checkpint config

--- AMINO/configs/callbacks.py
+++ AMINO/configs/callbacks.py
@@ -16,7 +16,7 @@ class MODELCHECKPOINT_CONF(ABC):
     monitor: str = 'val_normal_loss'
     save_last: bool = True
     save_top_k: int = 5
-    dirpath: str = '${expbase.exp}/${hydra:job.name}'
+    dirpath: str = 'checkpoint'
 
 @type_checked_constructor()
 @dataclass
