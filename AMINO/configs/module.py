f18307f3a49f24d1a42916afe575eeac888dbc29
qmpzzpmq <405691733@qq.com>
2021-11-15 11:03:27 +0800

    update scheduler

--- AMINO/configs/module.py
+++ AMINO/configs/module.py
@@ -15,10 +15,13 @@ class OPTIM(AMINO_CONF, ABC):
 class MODULE_CONF(ABC):
     loss_conf: AMINO_CONF = AMINO_CONF(select="torch.nn:MSELoss", conf={"reduction": "none"})
     optim_conf: OPTIM = OPTIM(
-        select="torch.optim:Adam", contiguous_params=False, conf={"lr": 0.001}
+        select="torch.optim:Adam", 
+        contiguous_params=False,
+        conf={"lr": 0.01}
     )
     scheduler_conf: AMINO_CONF = AMINO_CONF(
-        select="lambdalr", conf={"last_epoch": -1, "lr_lambda":'lambda epoch: 0.95 ** epoch'}
+        select="torch.optim.lr_scheduler:StepLR",
+        conf={"step_size": 5, "gamma": 0.1}
     )
     net_conf: AMINO_CONF = AMINO_CONF(
         select="AMINO.modules.nets.autoencoder:simple_autoencoder",
--- AMINO/modules/scheduler.py
+++ AMINO/modules/scheduler.py
@@ -1,15 +1,20 @@
 import torch
 
+from AMINO.utils.dynamic_import import dynamic_import
+
 def init_scheduler(optim, scheduler_conf):
-    scheduler_select = scheduler_conf.get('scheduler_select', "lambdalr")
-    if scheduler_select == "lambdalr":
-        scheduler_conf = dict(scheduler_conf['conf'])
-        lr_lambda = scheduler_conf.pop('lr_lambda')
-        lr_lambda = eval(lr_lambda)
-        return torch.optim.lr_scheduler.LambdaLR(
-            optim,
-            lr_lambda=lr_lambda,
-            **scheduler_conf,
-        )
-    else:
-        raise NotImplementedError(f"the scheduler policy {scheduler_select}")
+    # scheduler_select = scheduler_conf.get('scheduler_select', "lambdalr")
+    # if scheduler_select == "lambdalr":
+    #     scheduler_conf = dict(scheduler_conf['conf'])
+    #     lr_lambda = scheduler_conf.pop('lr_lambda')
+    #     lr_lambda = eval(lr_lambda)
+    #     return torch.optim.lr_scheduler.LambdaLR(
+    #         optim,
+    #         lr_lambda=lr_lambda,
+    #         **scheduler_conf,
+    #     )
+    # else:
+    #     raise NotImplementedError(f"the scheduler policy {scheduler_select}")
+    scheduler_class = dynamic_import(scheduler_conf['select'])
+    scheduler = scheduler_class(optim, **scheduler_conf['conf'])
+    return scheduler
